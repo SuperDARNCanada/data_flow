340ff7c5b0407afdd6f2701d8b3f93bf5a49bce0
radar <radar@borealis.usask.ca>
2021-05-11 22:29:04 +0000

    Removed Marci's email address

--- site-linux/convert_and_restructure_daily_on_nas.sh
+++ site-linux/convert_and_restructure_daily_on_nas.sh
@@ -53,7 +53,7 @@ send_email () {
         # Argument 1 should be the subject
         # Argument 2 should be the body
         # What email address to send to?
-        EMAILADDRESS="kevin.krieger@usask.ca, marci.detwiller@usask.ca"
+        EMAILADDRESS="kevin.krieger@usask.ca"
         echo -e "${2}" | mutt -s "${1}" -- ${EMAILADDRESS}
 }
 
