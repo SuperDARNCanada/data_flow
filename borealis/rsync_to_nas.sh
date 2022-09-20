b409bccbca964fb9228f9e70d2c0324ab75ece5a
Theodore Kolkman <tjk584@usask.ca>
2022-09-20 14:45:08 -0600

    Refactored rsync_to_nas
    - Made singleton check more succint
    - Cleaned up code
    - Replaced deprecated practices (mainly backticks)

--- borealis/rsync_to_nas.sh
+++ borealis/rsync_to_nas.sh
@@ -1,52 +1,64 @@
 #!/bin/bash
 # Copyright 2019 SuperDARN Canada, University of Saskatchewan
 # Author: Kevin Krieger
-
-# A singleton script to copy the data FILES to the on-site linux computer, removing them locally after
-# a successful copy. Dependencies include 'jq' (installed via zypper), 
-# as well as BOREALISPATH and SITE_LINUX being in the environment variables in $HOME/.profile
 #
+# Last edited: September 2022 by Theodore Kolkman
+#
+# A singleton script to copy the data FILES to the on-site linux computer, 
+# removing them locally after a successful copy. 
+#
+# Dependencies: 
+#	- jq (installed via zypper)
+# 	- BOREALISPATH and SITE_LINUX set as environment variables in $HOME/.profile
+#
+# TODO: Update when inotify method is completed.
 # The script should be run via crontab like so:
 # 8,45 */2 * * * . $HOME/.profile; $HOME/data_flow/rsync_to_linux.sh >> $HOME/rsync_to_linux.log 2>&1
 
-# What is this script called?
-SCRIPT_NAME=`basename "$0"`
-DEST=/borealis_nfs/borealis_data/daily/
-# Use jq with the raw output option and the Borealis directory to get the source for the data
-SOURCE=`cat ${BOREALISPATH}/config.ini | jq -r '.data_directory'`
-# The following constant is how many minutes threshold the script will use to find FILES
-# to move to the site linux computer. This is so that the script doesn't try to move the current data
-# file being written to.
-CUR_FILE_THRESHOLD_MINUTES=5
-# A temp directory for rsync to use in case rsync is killed, it will start up where it left off
-TEMPDEST=.rsync_partial
-# Where should the md5sum file live to verify the rsync transfer?
-MD5=/tmp/md5
 
-echo ${SCRIPT_NAME}
-
-# Date in UTC format for logging
-date -u
-echo "Placing FILES in ${DEST}"
-
-# Check to make sure this is the only instance running.
-# If only this one is running, will be ${HOME}/${SCRIPT_NAME} ${HOME}/${SCRIPT_NAME}
-RSYNCRUNNING="`ps aux | grep rsync_to_nas.sh | awk '$11 !~ /grep/ {print $12}'`" 
-
-#must be three times because the first two will be this instance of ${SCRIPT_NAME}
-if [[ "$RSYNCRUNNING" == *"${SCRIPT_NAME}"*"${SCRIPT_NAME}"*"${SCRIPT_NAME}"* ]]
-then 
-	echo "Another instance running, exiting"
-	exit
+# Ensure that only a single instance of this script runs.
+if pidof -o %PPID -x -- "$(basename -- $0)" > /dev/null; then
+	echo "Error: Script $0 is already running."
+	exit 1
 fi
 
-FILES=`find ${SOURCE} \( -name '*rawacf.hdf5.*' -o -name '*bfiq.hdf5.*' -o -name '*antennas_iq.hdf5.*' \) -cmin +${CUR_FILE_THRESHOLD_MINUTES} -printf '%p\n'`
-echo $FILES
-for file in $FILES
+
+# Define variables
+SCRIPT_NAME=$(basename "$0")
+
+# Directory the files will be transferred to
+readonly DEST=/borealis_nfs/borealis_data/daily/
+
+# Directory
+# Use jq with -r option source for the data from Borealis config file
+readonly SOURCE=$(cat ${BOREALISPATH}/config.ini | jq -r '.data_directory')
+
+# Log file
+readonly LOGFILE=/home/radar/logs/rsync_to_nas.log
+
+# Threshold (in minutes) for selecting files to transfer with `find`.
+# Set to filter out files currently being written to by Borealis.
+FILE_THRESHOLD=0.1
+
+# A temp directory for rsync to use in case rsync is killed, it will start up where it left off
+TEMPDEST=.rsync_partial
+
+# Directory of md5sum file to verify rsync transfer
+MD5=/tmp/md5
+
+# Date in UTC format for logging
+echo ${SCRIPT_NAME} >> $LOGFILE 2>&1
+date -u >> $LOGFILE 2>&1
+
+files=$(find ${SOURCE} \( -name '*rawacf.hdf5.*' -o -name '*bfiq.hdf5.*' -o -name '*antennas_iq.hdf5.*' \) -cmin +${FILE_THRESHOLD})
+
+echo "Placing following files in ${DEST}:" >> $LOGFILE 2>&1
+printf '%s\n' "${files[@]}"
+for file in $files
 do
 	# Get the file name, directory it's in and rsync it
-	datafile=`basename $file`	
-	path=`dirname $file`
+	datafile=$(basename $file)	
+	path=$(dirname $file)
 	cd $path
 	rsync -av --partial --partial-dir=${TEMPDEST} --timeout=180 --rsh=ssh ${datafile} ${DEST}
         
@@ -54,7 +66,7 @@ do
 	md5sum -b ${DEST}${datafile} > ${MD5}
 	md5sum -c ${MD5}
 	mdstat=$?
-	if [ ${mdstat} -eq 0 ]
+	if [[ ${mdstat} -eq 0 ]]
        	then
 		echo "Deleting file: ${file}"
 		rm -v ${file}
