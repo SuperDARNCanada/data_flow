fe267aac3a2900f55fefe15d425ef2c6d1e2c5cb
Remington Rohel <rar129@usask.ca>
2023-08-11 18:06:30 +0000

    Fix an issue with boolean fields stemming from deepdish.

--- site-linux/remove_record.py
+++ site-linux/remove_record.py
@@ -13,8 +13,8 @@ If a record name is provided, only the given record will be removed.
 
 """
 import argparse
-import deepdish
 import h5py
+
 
 def usage_msg():
     """
@@ -40,30 +40,28 @@ def usage_msg():
 def remove_records_parser():
     parser = argparse.ArgumentParser(usage=usage_msg())
     parser.add_argument("borealis_site_file", help="Path to the array file that needs correction.")
-    parser.add_argument("--record_name", default='', help="Group name in the hdf5 file to remove (Borealis record name).")
+    # parser.add_argument("--record_name", default='', help="Group name in the hdf5 file to remove (Borealis record name).")
     return parser
+
 
 def find_borealis_sequence_errors(filename):
     """
     Removes any records in the file where the shape of the sequences data 
     is not the correct size for the given num_sequences. 
     """
-    records = sorted(deepdish.io.load(filename).keys())
-    for record_name in records:
-        groupname = '/' + record_name
-        data = deepdish.io.load(filename,group=groupname)
-        if data['sqn_timestamps'].shape[0]!=data['num_sequences']:
-            remove_record(filename, groupname)
+    with h5py.File(filename, 'a') as f:
+        records = sorted(list(f.keys()))
+        for record_name in records:
+            data = f[record_name]
+            if data['sqn_timestamps'].shape[0] != data['num_sequences']:
+                del f[record_name]
+                print(f'Deleted: {record_name}')
 
-def remove_record(file_1, groupname):
-    with h5py.File(file_1,  "a") as f:
-        del f[groupname]
-        print('Deleted: ', groupname)
 
 if __name__ == '__main__':
     parser = remove_records_parser()
     args = parser.parse_args()
-    if args.record_name:
-        remove_record(args.borealis_site_file, args.record_name)
-    else:
-        find_borealis_sequence_errors(args.borealis_site_file)
+    # if args.record_name:
+    #     remove_record(args.borealis_site_file, args.record_name)
+    # else:
+    find_borealis_sequence_errors(args.borealis_site_file)
