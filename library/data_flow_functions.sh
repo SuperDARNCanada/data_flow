5cd1c5f9cb89e17afc207af490fe1f65f486da39
Draven G <dtg558@usask.ca>
2023-12-19 13:15:22 -0600

    LOGFILE was a protected variable. changed it

--- library/data_flow_functions.sh
+++ library/data_flow_functions.sh
@@ -157,18 +157,18 @@ verify_transfer () {
 alert_slack() {
   message=$1
   webhook=$2
-  LOGFILE="${HOME}/logs/slack_dataflow_notif.log"
+  LOGFILE_SLACKALERT="${HOME}/logs/slack_dataflow_notif.log"
 
   NOW=$(date +'%Y%m%d %H:%M:%S')
   if [[ -z ${message} ]]; then
-    echo "${NOW} dataflow slack message error: Empty message attempted to be sent." | tee -a "${LOGFILE}"
+    echo "${NOW} dataflow slack message error: Empty message attempted to be sent." | tee -a "${LOGFILE_SLACKALERT}"
   elif [[ -z ${webhook} ]]; then
-    echo "${NOW} dataflow webhook error: No webhook was found. Attempted message was ${message}" | tee -a "${LOGFILE}"
+    echo "${NOW} dataflow webhook error: No webhook was found. Attempted message was ${message}" | tee -a "${LOGFILE_SLACKALERT}"
   fi
 
   curl --silent --header "Content-type: application/json" --data "{'text':'${message}'}" "${webhook}"
   result=$?
   if [[ ${result} -ne 0 ]]; then
-    echo "${NOW} attempt to curl to webhook ${webhook} failed with error: ${result} (see https://curl.se/libcurl/c/libcurl-errors.html)" | tee -a "${LOGFILE}"
+    echo "${NOW} attempt to curl to webhook ${webhook} failed with error: ${result} (see https://curl.se/libcurl/c/libcurl-errors.html)" | tee -a "${LOGFILE_SLACKALERT}"
   fi
 }
